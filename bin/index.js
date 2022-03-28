c7fcd9c694c0292431bb2411c36a31f18510f6c0
cap811 <58876414+cap811@users.noreply.github.com>
2022-03-28 20:59:17 +0300

    Initial commit.

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+/* This is the main entry point for the CLI.
+It is responsible for parsing the command line arguments,
+and then invoking the appropriate command. */
+
+// Import the CLI modules
+import chalk from 'chalk';
+import ora from 'ora';
+
+// Module to handle the command line arguments
+
+
+// Test command
+console.log(chalk.green('Starting the CLI...'));
